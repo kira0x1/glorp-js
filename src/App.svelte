0a3083818a39c4d2f3c958065b7451d82a7bc194
Kira <kira.eva@protonmail.com>
2026-02-21 21:13:07 +0000

    Create status grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
+  import Status from "./lib/Status.svelte";
+  import icon from "./assets/svelte.svg";
+  import glorp_shiny from "./assets/images/glorp_shiny.png";
 </script>
 
 <main>
   <div class="header">Glorp</div>
+  <div class="status-grid">
+    <Status text="meow" img={glorp_shiny} />
+    <Status text="meow" img={glorp_shiny} />
+    <Status text="meow" img={glorp_shiny} />
+    <Status text="meow" img={glorp_shiny} />
+  </div>
 </main>
+
+<style>
+  .status-grid {
+    gap: 10px;
+    margin: 1em auto;
+    width: 42vw;
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
---
+++ src/lib/Status.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  let { text = "", img = null } = $props();
+</script>
+
+<div class="status">
+  {#if img}
+    <div id="status-img">
+      <img src={img} alt="img" />
+    </div>
+  {/if}
+  <span id="status-text">{text}</span>
+</div>
+
+<style>
+  .status {
+    display: flex;
+    border: 6px inset #838383;
+
+    /* (header width / status count ) - gap / 2 */
+    width: calc((42vw / 2) - 5px);
+
+    height: 58px;
+    flex-direction: row;
+    background-color: rgb(110, 110, 110);
+    color: #ccc;
+    justify-content: space-between;
+    box-sizing: border-box;
+    padding: 0 6px;
+    padding-top: 6px;
+  }
+
+  #status-text {
+    color: #ccc;
+    flex-grow: 1;
+    font-size: 1.3rem;
+    padding: 2px;
+  }
+
+  img {
+    height: 100%;
+    display: block;
+    box-sizing: border-box;
+  }
+</style>
